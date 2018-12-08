415c9c6768f9db1e3229c92c055f09e024fa1c4e
zenb <39472250+lg10@users.noreply.github.com>
2018-12-08 23:09:23 +0800

    Add files via upload

---
+++ 分段测试/duoji/duoji.ino
@@ -0,0 +1,12 @@
+#include <Servo.h>// 声明调用Servo.h库
+Servo myservo;// 创建一个舵机对象
+int pos=0;
+void setup(){
+  myservo.attach(6);
+}
+void loop(){
+      myservo.write(180);
+       delay(1000);
+      myservo.write(-180);
+      delay(1000);
+}
---
+++ 分段测试/hongwai/hongwai.ino
@@ -0,0 +1,23 @@
+#include <IRremote.h>     // IRremote库声明  
+  
+int RECV_PIN = 5;        //定义红外接收器的引脚为11  
+  
+IRrecv irrecv(RECV_PIN);   
+  
+decode_results results;   //解码结果放在 decode results结构的 result中
+    
+void setup()  
+  {  
+      Serial.begin(9600);  
+      irrecv.enableIRIn(); // 启动接收器  
+  }  
+  
+void loop() {  
+    if (irrecv.decode(&results))//解码成功，收到一组红外讯号   
+    {  
+      Serial.println(results.value, HEX);//以16进制换行输出接收代码  
+      irrecv.resume(); // 接收下一个值  
+    }  
+    delay(100);  
+  }  
+
